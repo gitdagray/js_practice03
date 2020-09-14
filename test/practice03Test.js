aac7c664562556ea9b7789f2438a689fbbd9a4b2
gitdagray <DaveOnEleven@gmail.com>
2020-09-14 12:53:36 -0500

    test update

--- test/practice03Test.js
+++ test/practice03Test.js
@@ -3,53 +3,53 @@ const assert = chai.assert;
 describe("Practice Assignment 3", function () {
   describe("Question 1", function () {
     it("The variable myDept should not have a value if the variable myNumber is not greater than 1.", function () {
-      let result = question1(1);
+      const result = question1(1);
       assert.notExists(result);
     });
     it("The variable myDept should hold the required value if the variable myNumber is greater than 1.", function () {
-      let result = question1(2);
+      const result = question1(2);
       assert.match(result, /^Informatics$/);
     });
   });
   describe("Question 2", function () {
     it("The variable myLang should hold the required value if the variable myValue has a length property equal to 10.", function () {
-      let result = question2("JavaScript");
+      const result = question2("JavaScript");
       assert.match(result, /^JavaScript$/);
     });
     it("The variable myLang should hold the required value if the variable myValue does not have a length property equal to 10.", function () {
-      let result = question2("Python");
+      const result = question2("Python");
       assert.match(result, /^Python$/);
     });
   });
   describe("Question 3", function () {
     it("The variable myLang should hold the required value if the variable myValue has a length property equal to 10.", function () {
-      let result = question3("JavaScript");
+      const result = question3("JavaScript");
       assert.match(result, /^JavaScript$/);
     });
     it("The variable myLang should hold the required value if the variable myValue has a length property greater than 3.", function () {
-      let result = question3("Dave");
+      const result = question3("Dave");
       assert.match(result, /^PHP$/);
     });
     it("The variable myLang should hold the required value if the variable myValue does not have a length property equal to 10.", function () {
-      let result = question3("Hey");
+      const result = question3("Hey");
       assert.match(result, /^Python$/);
     });
   });
   describe("Question 4", function () {
     it("The variable myFaveFood should hold the required value if the variable myNumber is equal to 1.", function () {
-      let result = question4(1);
+      const result = question4(1);
       assert.match(result, /^Pizza$/);
     });
     it("The variable myFaveFood should hold the required value if the variable myNumber is equal to 2.", function () {
-      let result = question4(2);
+      const result = question4(2);
       assert.match(result, /^Hamburger$/);
     });
     it("The variable myFaveFood should hold the required value if the variable myNumber is equal to 3.", function () {
-      let result = question4(3);
+      const result = question4(3);
       assert.match(result, /^Ice Cream$/);
     });
     it("The variable myFaveFood should hold the required value if the variable myNumber is equal to 4.", function () {
-      let result = question4(4);
+      const result = question4(4);
       assert.match(result, /^Sushi$/);
     });
     it("The variable myFaveFood should hold the required value if the variable myNumber is anything other than the values 1 through 4.", function () {
@@ -61,15 +61,15 @@ describe("Practice Assignment 3", function () {
   });
   describe("Question 5", function () {
     it("The variable myWeather should hold the required value if the variable myTemp has a value greater than 75.", function () {
-      let result = question5(76);
+      const result = question5(76);
       assert.match(result, /^Great weather!$/);
     });
     it("The variable myWeather should hold the required value if the variable myTemp has a value equal to 75.", function () {
-      let result = question5(75);
+      const result = question5(75);
       assert.match(result, /^Great weather!$/);
     });
     it("The variable myWeather should hold the required value if the variable myTemp has a value less than 75.", function () {
-      let result = question5(74);
+      const result = question5(74);
       assert.match(result, /^Still cold.$/);
     });
   });
